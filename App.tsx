2344fb0028ad7a74d7eba0fe8f2aa19ceded1edf
Nicholas Marques <nnmarques97@gmail.com>
2022-06-06 10:10:55 -0300

    feat: add new fonts

--- App.tsx
+++ App.tsx
@@ -6,10 +6,19 @@ import useColorScheme from "./src/hooks/useColorScheme";
 import Navigation from "./src/navigation";
 import { ThemeProvider } from "styled-components/native";
 import theme from "./src/styles/theme";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
+import { Text } from "react-native";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
 
   if (!isLoadingComplete) {
     return null;
