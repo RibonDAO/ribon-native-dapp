75627a9f1087f19e55bb419dc714392c99041a74
Nicholas Marques <nnmarques97@gmail.com>
2022-08-08 10:53:16 -0300

    feat: add non profits donation stories

--- src/screens/ModalScreen.tsx
+++ src/screens/ModalScreen.tsx
@@ -4,16 +4,19 @@ import { Platform, StyleSheet } from "react-native";
 
 import { Text, View } from "../components/Themed";
 
-export default function ModalScreen() {
+export default function ModalScreen({ route }) {
+  const { nonProfit } = route.params;
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
+      <Text style={styles.title}>Coloque seu email para doar</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
 
+      <Text>Doar para {nonProfit.name}</Text>
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
