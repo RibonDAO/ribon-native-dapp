f0467ffaf9abcda3865b243ec975dfa0602dc478
Nicholas Marques <nnmarques97@gmail.com>
2022-12-12 13:59:08 -0300

    chore: reset givings page

--- src/screens/GivingsPage/index.tsx
+++ src/screens/GivingsPage/index.tsx
@@ -1,28 +1,41 @@
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import useNonProfits from "@ribon/shared/src/hooks/apiHooks/useNonProfits";
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import theme from "styles/theme";
 
-function GivingsPage() {
-  const { nonProfits } = useNonProfits();
-
-  useEffect(() => {
-    console.log("asdasdasd,", nonProfits);
-    console.log(process.env);
-  }, [nonProfits]);
-
-  return (
-    <View style={styles.mainContainer}>
-      <Text>Salve</Text>
-      {nonProfits &&
-        nonProfits.map((nonProfit) => <Text>{nonProfit.name}</Text>)}
-    </View>
-  );
+export default class GivingsPage extends Component {
+  render() {
+    return <View style={styles.mainContainer}></View>;
+  }
 }
 
+let CIRCLE_RADIUS = 30;
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
   },
+  ballContainer: {
+    height: 200,
+  },
+  circle: {
+    backgroundColor: "skyblue",
+    width: CIRCLE_RADIUS * 2,
+    height: CIRCLE_RADIUS * 2,
+    borderRadius: CIRCLE_RADIUS,
+  },
+  row: {
+    flexDirection: "row",
+  },
+  dropZone: {
+    height: 200,
+    backgroundColor: theme.colors.ribonBlue,
+  },
+  text: {
+    marginTop: 25,
+    marginLeft: 5,
+    marginRight: 5,
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 25,
+    fontWeight: "bold",
+  },
 });
-
-export default GivingsPage;
