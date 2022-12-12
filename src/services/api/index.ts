b83b52b122f3f6cde9ab0d18109dd74e6a8869f7
Nicholas Marques <nnmarques97@gmail.com>
2022-12-12 11:31:08 -0300

    chore: update api service

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,35 +1,17 @@
-import Axios, { AxiosRequestConfig } from "axios";
-import camelCaseKeys from "camelcase-keys";
-import snakeCaseKeys from "snakecase-keys";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { CURRENT_USER_KEY } from "../../contexts/currentUserContext";
+import api, {
+  apiGet,
+  apiDelete,
+  apiPut,
+  apiPost,
+} from "@ribon/shared/src/services/api";
 
-const RIBON_API = "https://dapp-api.ribon.io/";
+const RIBON_API = "https://dapp-dev-api.ribon.io/";
 
 export const baseURL = process.env.REACT_APP_RIBON_API || RIBON_API;
-export const API_SCOPE = "/api/v1";
 
-const api = Axios.create({
-  baseURL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  validateStatus: (status) => status >= 200 && status < 300,
-});
-
-api.interceptors.request.use((request) =>
-  request?.data
-    ? { ...request, data: snakeCaseKeys(request?.data, { deep: true }) }
-    : request,
-);
-
-api.interceptors.response.use(
-  (response) => ({
-    ...response,
-    data: camelCaseKeys(response.data, { deep: true }),
-  }),
-  (error) => Promise.reject(error),
-);
+api.defaults.baseURL = baseURL;
 
 api.interceptors.request.use(async (config) => {
   const lang = "en";
@@ -42,28 +24,5 @@ api.interceptors.request.use(async (config) => {
   return config;
 });
 
-export function apiGet(url: string, config?: AxiosRequestConfig) {
-  if (config) return api.get(`${API_SCOPE}/${url}`, config);
-
-  return api.get(`${API_SCOPE}/${url}`);
-}
-
-export function apiPost(url: string, data: any, config?: AxiosRequestConfig) {
-  if (config) return api.post(`${API_SCOPE}/${url}`, data, config);
-
-  return api.post(`${API_SCOPE}/${url}`, data);
-}
-
-export function apiPut(url: string, data: any, config?: AxiosRequestConfig) {
-  if (config) return api.put(`${API_SCOPE}/${url}`, data, config);
-
-  return api.put(`${API_SCOPE}/${url}`, data);
-}
-
-export function apiDelete(url: string, config?: AxiosRequestConfig) {
-  if (config) return api.delete(`${API_SCOPE}/${url}`, config);
-
-  return api.delete(`${API_SCOPE}/${url}`);
-}
-
+export { apiGet, apiDelete, apiPut, apiPost };
 export default api;
